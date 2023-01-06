86d136481c961d7ed262a1a9810173d5e963480b
KhailoAndrey <hap512@gmail.com>
2023-01-07 01:43:59 +0200

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -7,14 +7,11 @@ const ingredients = [
   'Condiments',
 ];
 const list = document.querySelector("#ingredients");
-const arrEl = [];
-ingredients.forEach((ing) => {
+const arrEl = ingredients.map(ing => {
   const ingEl = document.createElement('li');
   ingEl.textContent = `${ing}`;
   ingEl.classList.add('item');
-  arrEl.push(ingEl);
+  return ingEl;
 });
-// arrEl.forEach((el) => list.appendChild(el));
-// console.log(arrEl);
 list.append(...arrEl);
 console.log(list);

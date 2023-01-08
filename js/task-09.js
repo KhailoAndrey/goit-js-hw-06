186421ed7673796ae3ed4453a3d6ec2b42e6305b
KhailoAndrey <hap512@gmail.com>
2023-01-08 17:26:45 +0200

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,15 +1,12 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
 }
 
 const changeColorBtn = document.querySelector(".change-color");
-console.log(changeColorBtn);
-
 const spanColor = document.querySelector(".color");
-console.log(spanColor);
-
 const bodyColor = document.querySelector("body");
-console.log(bodyColor);
 
 changeColorBtn.addEventListener("click", () => {
   const randomColor = getRandomHexColor();

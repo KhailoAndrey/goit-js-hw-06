5a10c934192916e82aa72ccd26e5fe7e8dccbff3
KhailoAndrey <hap512@gmail.com>
2023-01-08 16:56:47 +0200

    Task-9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,18 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const changeColorBtn = document.querySelector(".change-color");
+console.log(changeColorBtn);
+
+const spanColor = document.querySelector(".color");
+console.log(spanColor);
+
+const bodyColor = document.querySelector("body");
+console.log(bodyColor);
+
+changeColorBtn.addEventListener("click", () => {
+  const randomColor = getRandomHexColor();
+  spanColor.textContent = `- ${randomColor}`;
+  bodyColor.style.backgroundColor = randomColor;
+});
